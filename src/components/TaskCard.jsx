34106d1d7dcfd6e175a47e9910bc21743a2eb206
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-29 23:29:19 +0530

    added TaskCard

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,5 +1,10 @@
 const TaskCard = () => {
-  return <div>TaskCard</div>;
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-md mb-4">
+      <h3 className="text-lg font-semibold">Title</h3>
+      <p className="text-sm text-gray-600">Description...</p>
+    </div>
+  );
 };
 
 export default TaskCard;
