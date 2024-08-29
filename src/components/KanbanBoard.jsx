def41bd64158ee3af67ce744779dfe7456878ee7
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-29 23:19:49 +0530

    styled kanbanBoard

--- src/components/KanbanBoard.jsx
+++ src/components/KanbanBoard.jsx
@@ -1,18 +1,22 @@
-import React from "react";
-
 const KanbanBoard = () => {
   return (
-    <div>
-      <div>
-        <h2>To Do</h2>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4 text-center">
+      <div className="bg-red-100 p-4 rounded-lg shadow-md">
+        <h2 className="text-xl font-semibold text-red-800 mb-4">To Do</h2>
       </div>
-      <div>
-        <h2>In Progress</h2>
+      <div className="bg-blue-100 p-4 rounded-lg shadow-md">
+        <h2 className="text-xl font-semibold text-blue-800 mb-4">
+          In Progress
+        </h2>
       </div>
-      <div>
-        <h2>Peer Review</h2>
+      <div className="bg-yellow-100 p-4 rounded-lg shadow-md">
+        <h2 className="text-xl font-semibold text-yellow-800 mb-4">
+          Peer Review
+        </h2>
       </div>
-      <div>Done</div>
+      <div className="bg-green-100 p-4 rounded-lg shadow-md">
+        <h2 className="text-xl font-semibold text-green-800 mb-4">Done</h2>
+      </div>
     </div>
   );
 };
