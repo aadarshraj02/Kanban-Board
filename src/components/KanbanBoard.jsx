091818b840ec8717b62d5ad8763187c818a0e673
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-29 22:53:37 +0530

    added basic structure

--- src/components/KanbanBoard.jsx
+++ src/components/KanbanBoard.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const KanbanBoard = () => {
-  return <div>KanbanBoard</div>;
+  return (
+    <div>
+      <div>
+        <h2>To Do</h2>
+      </div>
+      <div>
+        <h2>In Progress</h2>
+      </div>
+      <div>
+        <h2>Peer Review</h2>
+      </div>
+      <div>Done</div>
+    </div>
+  );
 };
 
 export default KanbanBoard;
