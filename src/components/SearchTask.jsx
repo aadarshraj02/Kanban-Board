5e4f22e5f14cfe5d7f8933851c94eb8df4c3d254
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-30 20:33:57 +0530

    added searc h input

--- src/components/SearchTask.jsx
+++ src/components/SearchTask.jsx
@@ -1,7 +1,13 @@
 const SearchTask = () => {
-  return <div className="flex justify-center">
-    <input type="text" className="px-2 py-1 outline-none rounded-lg" />
-  </div>;
+  return (
+    <div className="flex px-4 sm:justify-center">
+      <input
+        type="text"
+        className="px-3 py-1.5 outline-none rounded-lg w-[75vw] sm:w-[60vw] md:w-[50vw] lg:w-[40vw] xl:w-[30vw] text-center "
+        placeholder="Search Task here.."
+      />
+    </div>
+  );
 };
 
 export default SearchTask;
