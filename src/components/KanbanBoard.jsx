9c8c5525aaf5b2f2d3f07a22161adc9ebe7c8a6d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-30 10:46:13 +0530

    passed as props and displayed tasks dynamically

--- src/components/KanbanBoard.jsx
+++ src/components/KanbanBoard.jsx
@@ -1,5 +1,20 @@
 import TaskCard from "./TaskCard";
 
+const allTasks = {
+  todo: [
+    {
+      id: 1,
+      title: "Task 1",
+      description:
+        "Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. ",
+    },
+    { id: 2, title: "Task 2", description: "Description" },
+  ],
+  inProgress: [{ id: 3, title: "Task 3", description: "Description" }],
+  peerReview: [{ id: 4, title: "Task 4", description: "Description" }],
+  done: [{ id: 5, title: "Task 5", description: "Description" }],
+};
+
 const KanbanBoard = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4 text-center">
@@ -7,25 +22,49 @@ const KanbanBoard = () => {
         <h2 className="text-xl font-semibold text-red-800 bg-red-100 mb-2 p-4 rounded-lg">
           To Do
         </h2>
-        <TaskCard />
+        {allTasks.todo.map((task) => (
+          <TaskCard
+            key={task.id}
+            title={task.title}
+            description={task.description}
+          />
+        ))}
       </div>
       <div className="border-r-2 border-zinc-500 pr-3">
         <h2 className="text-xl font-semibold text-blue-800 mb-2 bg-blue-100 p-4 rounded-lg">
           In Progress
         </h2>
-        <TaskCard />
+        {allTasks.inProgress.map((task) => (
+          <TaskCard
+            key={task.id}
+            title={task.title}
+            description={task.description}
+          />
+        ))}
       </div>
       <div className="border-r-2 border-zinc-500 pr-3">
         <h2 className="text-xl font-semibold text-yellow-800 mb-2 bg-yellow-100 p-4 rounded-lg">
           Peer Review
         </h2>
-        <TaskCard />
+        {allTasks.peerReview.map((task) => (
+          <TaskCard
+            key={task.id}
+            title={task.title}
+            description={task.description}
+          />
+        ))}
       </div>
-      <div className="">
+      <div>
         <h2 className="text-xl font-semibold text-green-800 mb-2 bg-green-100 p-4 rounded-lg">
           Done
         </h2>
-        <TaskCard />
+        {allTasks.done.map((task) => (
+          <TaskCard
+            key={task.id}
+            title={task.title}
+            description={task.description}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,8 +1,15 @@
-const TaskCard = () => {
+const TaskCard = ({ title, description }) => {
+  const shortenedDescription =
+    description.length > 100
+      ? `${description.substring(0, 100)}...`
+      : description;
+
   return (
-    <div className="bg-white p-4 rounded-lg shadow-md mb-4">
-      <h3 className="text-lg font-semibold">Title</h3>
-      <p className="text-sm text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic deleniti in architecto non optio ullam adipisci fuga ducimus eligendi repellendus.</p>
+    <div className="bg-white p-4 rounded-lg shadow-md mb-4 w-full h-32 flex flex-col gap-1">
+      <h3 className="text-lg font-semibold">{title}</h3>
+      <p className="text-sm text-gray-600 overflow-hidden">
+        {shortenedDescription}
+      </p>
     </div>
   );
 };
