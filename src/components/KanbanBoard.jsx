de1cd92e1bc2ca85e03b091136d8e69a7dbf5dfd
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-30 10:29:14 +0530

    fixed layout

--- src/components/KanbanBoard.jsx
+++ src/components/KanbanBoard.jsx
@@ -3,24 +3,28 @@ import TaskCard from "./TaskCard";
 const KanbanBoard = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4 text-center">
-      <div className="bg-red-100 p-4 rounded-lg shadow-md">
-        <h2 className="text-xl font-semibold text-red-800 mb-4">To Do</h2>
+      <div className="border-r-2 border-zinc-500 pr-3">
+        <h2 className="text-xl font-semibold text-red-800 bg-red-100 mb-2 p-4 rounded-lg">
+          To Do
+        </h2>
         <TaskCard />
       </div>
-      <div className="bg-blue-100 p-4 rounded-lg shadow-md">
-        <h2 className="text-xl font-semibold text-blue-800 mb-4">
+      <div className="border-r-2 border-zinc-500 pr-3">
+        <h2 className="text-xl font-semibold text-blue-800 mb-2 bg-blue-100 p-4 rounded-lg">
           In Progress
         </h2>
         <TaskCard />
       </div>
-      <div className="bg-yellow-100 p-4 rounded-lg shadow-md">
-        <h2 className="text-xl font-semibold text-yellow-800 mb-4">
+      <div className="border-r-2 border-zinc-500 pr-3">
+        <h2 className="text-xl font-semibold text-yellow-800 mb-2 bg-yellow-100 p-4 rounded-lg">
           Peer Review
         </h2>
         <TaskCard />
       </div>
-      <div className="bg-green-100 p-4 rounded-lg shadow-md">
-        <h2 className="text-xl font-semibold text-green-800 mb-4">Done</h2>
+      <div className="">
+        <h2 className="text-xl font-semibold text-green-800 mb-2 bg-green-100 p-4 rounded-lg">
+          Done
+        </h2>
         <TaskCard />
       </div>
     </div>
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -2,7 +2,7 @@ const TaskCard = () => {
   return (
     <div className="bg-white p-4 rounded-lg shadow-md mb-4">
       <h3 className="text-lg font-semibold">Title</h3>
-      <p className="text-sm text-gray-600">Description...</p>
+      <p className="text-sm text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic deleniti in architecto non optio ullam adipisci fuga ducimus eligendi repellendus.</p>
     </div>
   );
 };
