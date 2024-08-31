0863164f18665f963528d8eee68317d99ba561e0
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-31 11:56:32 +0530

    added basic form layout for update task

--- src/components/EditTask.jsx
+++ src/components/EditTask.jsx
@@ -1,5 +1,36 @@
 const EditTask = () => {
-  return <div>EditTask</div>;
+  return (
+    <div>
+      <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+        <div className="bg-zinc-700 p-6 rounded-lg shadow-lg w-96">
+          <h2 className="text-xl text-white font-semibold mb-4">
+            Add New Task
+          </h2>
+          <input
+            type="text"
+            placeholder="Task Title"
+            className="w-full p-2 mb-4 border border-gray-300 rounded outline-none"
+          />
+          <textarea
+            placeholder="Task Description"
+            className="w-full p-2 mb-4 border border-gray-300 rounded h-24 outline-none"
+          />
+          <div className="flex justify-end gap-2">
+            <button
+              className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+            >
+              Cancel
+            </button>
+            <button
+              className="bg-green-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            >
+              Update Task
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EditTask;
